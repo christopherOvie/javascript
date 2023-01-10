96fa2e853ed137329f9dab99a35a62d6d4b537b3
unknown <covie@planittesting.com>
2023-01-10 11:25:10 +0000

    first commit

---
+++ section22/stringConversionArray.js
@@ -0,0 +1,45 @@
+//splitting
+var str= '1, 2, 3, 4, 5'
+var arr=str.split(',')
+
+var str ='12345'
+
+var arr=str.split('2')
+
+var str ='- high -'
+var arr = str.split('-')
+console.log(arr)
+
+var str ='i have a puppy'
+var arr =str.split('h')
+
+var str ='i have a puppy'
+var arr =str.split(' ')
+
+
+//convert string to number
+var num = '5'
+var NewNum =Number(num)
+
+
+var numb= "342"
+var MyNewNumber=Number(numb)
+
+ //MyNewNumber =Number('5.5')
+ MyNewNumber=Number(' 5.9')
+
+ var numb= "342"
+var MyNewNumber=Number(numb)
+
+ //MyNewNumber =Number('5.5')
+ //MyNewNumber=Number(' 5.9')
+ MyNewNumber=Number('t5')
+
+
+
+ var num = ''
+ //num= 
+
+
+
+
